6d083a3b010cc389de0b5c3973598267807c206a
Jose M Marin Armenteros <jose.marinarmenteros@santander.co.uk>
2018-10-13 09:20:16 +0100

    Automated commit

--- pmat.main.js
+++ pmat.main.js
@@ -15,7 +15,8 @@ try {
     pmat.api.loader()
 } catch (e) {
     console.log('failed to execute pmat.api.loader: exception name: ' + e.name + ' message: ' + e.message)
-
+    console.log('testCases: ', testCases)
+    
     pm.test('Critital ERROR!!!! There have been exceptions... take a look to the console logs to see them...',
         function () {
             pm.expect(0).to.equal(1)
